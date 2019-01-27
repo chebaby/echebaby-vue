9f887b2c377bb92651fc718abef747f2fe9258ab
Nour-Eddine ECH-CHEBABY <ech.chebaby@outlook.com>
2019-01-28 00:03:58 +0100

    [wip] setup protfolio popup

--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -11,8 +11,8 @@
             <div class="row">
                 <div class="col-sm-4 col-md-6 col-lg-4">
                     <div class="gallery-items wow fadeInLeft" data-wow-delay="0.2s">
-                        <a class="view img" href="<%= require('./assets/images/portfolio/fundbid.jpg') %>">  
-                            <img src="<%= require('./assets/images/portfolio/small/fundbid.png') %>" alt="portfolio image">
+                        <a class="view img" :href="fundbid.original">  
+                            <img :src="fundbid.small" alt="portfolio image">
                             
                         </a>
 
@@ -23,9 +23,9 @@
                 </div>
                 <div class="col-sm-4 col-md-6 col-lg-4">
                     <div class="gallery-items wow fadeInDown" data-wow-delay="0.4s">
-                        <a class="view img" href="<%= require('./assets/images/portfolio/doctori.jpg') %>">
+                        <a class="view img" href="@/assets/images/portfolio/doctori.jpg">
                             
-                            <img src="<%= require('./assets/images/portfolio/small/doctori.png') %>" alt="portfolio image"> 
+                            <img src="../assets/images/portfolio/small/doctori.png" alt="portfolio image"> 
                         </a>
 
                         <div class="gallery-text">
@@ -35,8 +35,8 @@
                 </div>
                 <div class="col-sm-4 col-md-6 col-lg-4">
                     <div class="gallery-items wow fadeInRight" data-wow-delay="0.6s">
-                        <a class="view img" href="<%= require('./assets/images/portfolio/tripdiem.jpg') %>"> 
-                            <img src="<%= require('./assets/images/portfolio/small/tripdiem.png') %>" alt="portfolio image"> 
+                        <a class="view img" href="@/assets/images/portfolio/tripdiem.jpg"> 
+                            <img src="../assets/images/portfolio/small/tripdiem.png" alt="portfolio image"> 
                         </a>
 
                         <div class="gallery-text">
@@ -46,8 +46,8 @@
                 </div>
                 <div class="col-sm-4 col-md-6 col-lg-4">
                     <div class="gallery-items wow fadeInLeft" data-wow-delay="0.8s">
-                        <a class="view img" href="<%= require('./assets/images/portfolio/fooz.jpg') %>"> 
-                            <img src="<%= require('./assets/images/portfolio/small/fooz.png') %>" alt="portfolio image"> 
+                        <a class="view img" href="@/assets/images/portfolio/fooz.jpg"> 
+                            <img src="../assets/images/portfolio/small/fooz.png" alt="portfolio image"> 
                         </a>
 
                         <div class="gallery-text">
@@ -57,8 +57,8 @@
                 </div>
                 <div class="col-sm-4 col-md-6 col-lg-4">
                     <div class="gallery-items wow fadeInDown" data-wow-delay="1s">
-                        <a class="view img" href="<%= require('./assets/images/portfolio/small/5.png') %>"> <!--edit image here-->
-                            <img src="<%= require('./assets/images/portfolio/small/5.png') %>" alt="portfolio image"> <!--edit image here-->
+                        <a class="view img" href="@/assets/images/portfolio/small/5.png"> <!--edit image here-->
+                            <img src="../assets/images/portfolio/small/5.png" alt="portfolio image"> <!--edit image here-->
                         </a>
 
                         <div class="gallery-text">
@@ -68,8 +68,8 @@
                 </div>
                 <div class="col-sm-4 col-md-6 col-lg-4">
                     <div class="gallery-items wow fadeInRight" data-wow-delay="1.2s">
-                        <a class="view img" href="<%= require('./assets/images/portfolio/small/6.png') %>"> <!--edit image here-->
-                            <img src="<%= require('./assets/images/portfolio/small/6.png') %>" alt="portfolio image"> <!--edit image here-->
+                        <a class="view img" href="@/assets/images/portfolio/6.png">
+                            <img src="../assets/images/portfolio/small/6.png" alt="portfolio image">
                         </a>
 
                         <div class="gallery-text">
@@ -83,5 +83,36 @@
 </template>
 
 <script>
-	export default {}
+    import '../assets/scripts/jquery.magnific-popup';
+	export default {
+
+        data() {
+
+            return {
+                fundbid : {
+                    original : require('../assets/images/portfolio/fundbid.jpg'),
+                    small : require('../assets/images/portfolio/small/fundbid.png')
+                }
+            }
+        },
+
+        mounted() {
+
+            console.log('%cPortfolio', 'background: #222; color: #bada55; padding: 4px;');
+
+            $('.view').magnificPopup({
+                type:'image',
+                gallery: {
+                    enabled: true
+                },
+                zoom: {
+                    enabled: true,
+                    duration: 300,
+                    opener: function(element) {
+                        return element.find('img');
+                    }
+                }
+            });
+        }
+    }
 </script>
